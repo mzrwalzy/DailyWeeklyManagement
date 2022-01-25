e4718380459f0b98f8aedc6b606cf377ac968ebf
MENG <mzrwalzy@163.com>
2022-01-25 16:40:37 +0800

    修改登陆界面

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-      <!-- <router-link to="/">去home组件</router-link> | 
+    <!-- <router-link to="/">去home组件</router-link> | 
       <router-link to="/about">去about组件</router-link> -->
-      <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: calc(100vh);
 }
-
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login">
+  <!-- <div class="login">
     <el-form
       :model="loginForm"
       status-icon
@@ -11,19 +11,62 @@
       <el-form-item label="用户名" prop="username">
         <el-input v-model="loginForm.username"></el-input>
       </el-form-item>
-      <el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input
-            type="password"
-            v-model="loginForm.password"
-            autocomplete="off"
-          ></el-input>
-        </el-form-item>
 
-        <el-button type="primary" @click="submitForm">提交</el-button>
+      <el-form-item label="密码" prop="password" class="passwordInput">
+        <el-input
+          type="password"
+          v-model="loginForm.password"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm" style="margin-top: 20px">
+          提交
+        </el-button>
         <el-button @click="resetForm">重置</el-button>
       </el-form-item>
     </el-form>
+  </div> -->
+  <div class="store-login">
+    <div class="bg-login">
+      <li v-for="bgimg in bgImages" :key="bgimg">
+        <img :src="bgimg" />
+      </li>
+    </div>
+    <div class="login-s">
+      <!-- 登录表单 -->
+      <el-form
+        :model="loginForm"
+        status-icon
+        ref="loginForm"
+        :rules="rules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user-solid"
+            placeholder="用户名"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="el-icon-key"
+            placeholder="密码"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="submitForm">登录</el-button>
+          <el-button type="info" @click="resetForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -43,6 +86,10 @@ export default {
       }
     };
     return {
+      bgImages: [
+        require("@/assets/images/bg1.jpg"),
+        require("@/assets/images/bg2.jpg"),
+      ],
       loginForm: {
         username: "",
         password: "",
@@ -87,11 +134,67 @@ export default {
     },
     resetForm() {
       this.loginForm.username = "";
-      htis.loginForm.password = "";
+      this.loginForm.password = "";
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.store-login {
+  background-color: white;
+  height: 100%;
+  position: relative;
+}
+
+.bg-login {
+  /*background: url("../../public/img/3Dbg01.jpg") no-repeat;*/
+  /*background-size: 100% 100%;*/
+  position: relative;
+  height: 100%;
+  opacity: 0.8;
+  z-index: 1;
+}
+
+.bg-login > li {
+  list-style: none;
+  /*display: none;*/
+  position: relative;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+.bg-login > li > img {
+  width: 100%;
+  height: 100%;
+  opacity: 0.9;
+  transition: ease 0.5s;
+}
+.login-s {
+  position: absolute;
+  width: 350px;
+  height: 400px;
+  background-color: rgba(0, 0, 0, 0.3);
+  top: 35%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 8px;
+  z-index: 2;
+  box-shadow: 10px 10px 20px 0px rgba(0, 0, 0, 0.7);
+  text-align: center;
+  opacity: 0.8;
+}
+
+.login_form {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 0 30px;
+}
 </style>
